d2e59fe631dca69ce302ba6fddc920298d29ee26
Danilo Castro <a25457@alunos.ipca.pt>
2025-12-04 23:53:34 +0000

    Testing API DB Server ok

---
+++ TP_ISI_02.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_ISI_02.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
